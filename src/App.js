9996da56b2fec45b9508390359233a1e49266691
Colin Birdwell <web.by.birdwell@gmail.com>
2018-03-18 22:46:38 -0500

    presentation

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.handleDeleteOption = this.handleDeleteOption.bind(this);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
     this.state = {
       options: []
     };
@@ -46,10 +46,12 @@ class App extends React.Component {
       options: prevState.options.filter((option) => optionToRemove !== option)
     }));
   }
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    const option = this.state.options[randomNum];
-    alert(option);
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility
+      };
+    });
   }
   handleAddOption(option) {
     if (!option) {
@@ -63,15 +65,12 @@ class App extends React.Component {
     }));
   }
   render() {
-    const subtitle = 'Put your life in the hands of a computer';
+    const subtitle = 'I make a simple list that lives in your Browser';
 
     return (
       <div className="app-body">
         <Header subtitle={subtitle} />
-        <Action
-          hasOptions={this.state.options.length > 0}
-          handlePick={this.handlePick}
-        />
+        
         <Options
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
@@ -80,6 +79,7 @@ class App extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+     
       </div>
     );
   }
@@ -95,20 +95,7 @@ const Header = (props) => {
 };
 
 Header.defaultProps = {
-  title: 'Indecision'
-};
-
-const Action = (props) => {
-  return (
-    <div>
-      <button
-        onClick={props.handlePick}
-        disabled={!props.hasOptions}
-      >
-        What should I do?
-      </button>
-    </div>
-  );
+  title: 'A Super Quick React Demo'
 };
 
 const Options = (props) => {
@@ -131,8 +118,10 @@ const Options = (props) => {
 
 const Option = (props) => {
   return (
-    <div>
+    <div className="option-item">
+    <p>
       {props.optionText}
+    </p>
       <button
         onClick={(e) => {
           props.handleDeleteOption(props.optionText);
@@ -144,15 +133,6 @@ const Option = (props) => {
   );
 };
 
-const Explain = () =>{
-  return(
-    <div>
-    <button>
-    
-    </button>
-    </div>
-  )
-};
 
 class AddOption extends React.Component {
   constructor(props) {
